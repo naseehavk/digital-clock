aca445b468743dff3287f150f5126646a2860bbb
naseeha saidu mohammed vk <naseehavk@gmail.com>
2024-06-05 13:20:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let section = document.querySelector("section")
+icons = document.querySelector(".icons")
+
+icons.onclick = () => {
+    section.classList.toggle("dark")
+}
+
+//creating a function and calling it in every seconds
+
+setInterval(() => {
+
+    let date = new Date(),
+        hour = date.getHours(),
+        min = date.getMinutes(),
+        sec = date.getSeconds();
+
+    let d;
+    d = hour > 12 ? "AM" : "PM";
+    hour = hour > 12 ? hour - 12 : hour;
+    hour = hour == 0 ? hour = 12 : hour;
+
+    hour = hour < 10 ? "0" + hour : hour ;
+    min = min < 10 ? "0" + min : min ;
+    sec = sec < 10 ? "0" + sec : sec ;
+
+
+    console.log(sec);
+    document.querySelector(".hour_num").innerText = hour;
+    document.querySelector(".min_num").innerText = min;
+    document.querySelector(".sec_num").innerText = sec;
+    document.querySelector(".am_pm").innerText = d;
+
+
+
+}, 1000);//1000 milliseconds =1s
